8a91f433796d34b5f51e5672816ee8ccbc299aba
Mike Shahindoust <gobias101@gmail.com>
2020-08-04 00:11:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def line(array)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    
+    position = katz_deli.index + 1
+    katz_deli.each do |num|
+      puts 
   end
 end
