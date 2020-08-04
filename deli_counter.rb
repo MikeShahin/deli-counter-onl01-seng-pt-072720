83aaee8a1c1434ea02f92f721c3fad4ebc112748
Mike Shahindoust <gobias101@gmail.com>
2020-08-04 00:24:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,4 +16,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts ""
+  position = katz_deli.length + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
+  return katz_deli
+end
