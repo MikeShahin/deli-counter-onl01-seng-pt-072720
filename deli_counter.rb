ae940f014589bee8ed250edba647ce9721c1925e
Mike Shahindoust <gobias101@gmail.com>
2020-08-04 00:21:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    position = katz_deli.index + 1
+    position = 1
     announce = []
     katz_deli.each do |name|
       announce.push("#{position}. #{name}")
+      position += 1
     end
     puts "The line is currently: #{announce.join(" ")}"
   end
